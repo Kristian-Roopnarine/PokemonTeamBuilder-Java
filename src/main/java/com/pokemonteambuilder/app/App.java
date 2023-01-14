12d5135ba7dac74a22b3d5d80dc8ec89d81f0648
kristian roopnarine <kristianroopnarine@kristians-MacBook-Pro.local>
2023-01-13 20:55:40 -0500

    pull html from pokedexdb

--- src/main/java/com/pokemonteambuilder/app/App.java
+++ src/main/java/com/pokemonteambuilder/app/App.java
@@ -5,12 +5,23 @@ package com.pokemonteambuilder.app;
  *
  */
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.IOException;
+
 public class App 
 {
-    private final static WebClient webClient = new WebClient();
+    private final static WebClient webClient = new WebClient(BrowserVersion.CHROME);
     public static void main( String[] args )
     {
-        System.out.println(webClient);
-        System.out.println( "Hello World!" );
+        final String URL = "https://pokemondb.net/pokedex/all";
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setCssEnabled(false);
+        try {
+            HtmlPage page = webClient.getPage(URL);
+            System.out.println(page.asXml());
+        } catch (IOException e ){
+            System.out.println("Error : " + e);
+        }
     }
 }
