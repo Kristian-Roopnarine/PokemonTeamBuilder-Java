2db377dbd7f6310acd809923649ddb73bbc4c73a
kristian roopnarine <kristianroopnarine@kristians-MBP.fios-router.home>
2023-01-15 13:11:50 -0500

    Add : code to save scraped results to csv

--- src/main/java/com/pokemonteambuilder/app/App.java
+++ src/main/java/com/pokemonteambuilder/app/App.java
@@ -15,36 +15,54 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.util.List;
 
-public class App 
-{
+public class App {
     private final static WebClient webClient = new WebClient(BrowserVersion.CHROME);
-    public static void main( String[] args )
-    {
-        final String URL = "https://pokemondb.net/pokedex/all";
-        webClient.getOptions().setJavaScriptEnabled(false);
-        webClient.getOptions().setCssEnabled(false);
-        String outputString = "";
-        try {
-            HtmlPage page = webClient.getPage(URL);
-            HtmlTable pokedexTable = page.getHtmlElementById("pokedex");
-            for (final HtmlTableRow row: pokedexTable.getRows()){
-                String rowString = "";
-                for(final HtmlTableCell cell: row.getCells()){
-                    rowString += cell.getTextContent() + ",";
 
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String firstArg = args[0];
+            // Can't use firstArg == SCRAPE because java will compare the references of the
+            // two string not the values
+            if (firstArg.equals("SCRAPE")) {
+                System.out.println("Scraping Pokemon data first.");
+                final String URL = "https://pokemondb.net/pokedex/all";
+                webClient.getOptions().setJavaScriptEnabled(false);
+                webClient.getOptions().setCssEnabled(false);
+                String outputString = "";
+                try {
+                    HtmlPage page = webClient.getPage(URL);
+                    HtmlTable pokedexTable = page.getHtmlElementById("pokedex");
+                    List<HtmlTableRow> rows = pokedexTable.getRows();
+                    // Example of the Row data
+                    // Number, Name (could have other form), Types (could be multiple)
+                    // Index 3-9 are the stats
+                    // 003,Venusaur Mega Venusaur,Grass Poison,625,80,100,123,122,120,80,
+                    for (int i = 1; i < rows.size(); i++) {
+                        HtmlTableRow currRow = rows.get(i);
+                        List<HtmlTableCell> currCells = currRow.getCells();
+                        String[] cellData = new String[currCells.size()];
+                        for (int j = 0; j < currCells.size(); j++) {
+                            cellData[j] = currCells.get(j).getTextContent();
+                        }
+                        String rowString = String.join(",", cellData);
+                        outputString += rowString + "\n";
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error : " + e);
                 }
-                outputString += rowString + "\n";
-            }
-        } catch (IOException e ){
-            System.out.println("Error : " + e);
-        }
 
-        final Path saveFilePath = Paths.get(System.getProperty("user.dir") + "/test_output.txt");
-        try {
-            Files.writeString(saveFilePath, outputString, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            System.out.println("Error saving data : " + e);
-        } 
+                final Path saveFilePath = Paths.get(System.getProperty("user.dir") + "/test_output.csv");
+                try {
+                    Files.writeString(saveFilePath, outputString, StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    System.out.println("Error saving data : " + e);
+                }
+                return;
+            }
+        } else {
+            System.out.println("No arguments passed to script. Will not scrape.");
+        }
     }
 }
